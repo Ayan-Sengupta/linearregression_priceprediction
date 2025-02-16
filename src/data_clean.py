59b64b5688a0e42167a63711f2e2145f229dffe0
Ayan Sengupta <asengupta.mlg@gmail.com>
2025-02-16 12:25:56 -0500

    betetr checking for missing values
    Signed-off-by: Ayan Sengupta <asengupta.mlg@gmail.com>

--- src/data_clean.py
+++ src/data_clean.py
@@ -26,21 +26,32 @@ listings_df = listings_df[listings_df['price'].notna()]
 # check if price column has any missing values 
 price_nullFlag = listings_df['price'].isnull().sum()
 
-# check if accommodates, bathrooms, bedrooms, minumum_nights and review_score_ratings columns have any missing values
-print(listings_df['accommodates'].isnull().sum())
-print(listings_df['bathrooms'].isnull().sum())
-print(listings_df['bedrooms'].isnull().sum())
-print(listings_df['minimum_nights'].isnull().sum())
-print(listings_df['review_scores_rating'].isnull().sum())
+
 
 # Create a dummy variable column to indicate if review_scores_rating is missing
 listings_df['has_review_scores'] = listings_df['review_scores_rating'].notna().astype(int)
 
+# replace missing values in review_scores_rating with a 0 
+listings_df['review_scores_rating'] = listings_df['review_scores_rating'].fillna(0)
 
+# check if accommodates, bathrooms, bedrooms, minumum_nights and review_score_ratings columns have any missing values
+missing_values = [
+    listings_df['accommodates'].isnull().sum(),
+    listings_df['bathrooms'].isnull().sum(),
+    listings_df['bedrooms'].isnull().sum(),
+    listings_df['minimum_nights'].isnull().sum(),
+    listings_df['review_scores_rating'].isnull().sum()
+]
 
+# loop thrrough missing values list and make sure each entry is 0 
+sum = 0
+for i in missing_values:
+    sum += i
+   
+print(sum)
 
 # if no missing value, write the df to a new csv file
-if price_nullFlag == 0:
+if price_nullFlag == 0 and sum == 0:
     listings_df.to_csv('data/processed/listings_clean.csv', index=False)
 
 
