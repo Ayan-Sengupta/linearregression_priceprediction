60b32b0ad4cf1c8611993b3cb1d53b9462453095
Ayan Sengupta <asengupta.mlg@gmail.com>
2025-02-16 13:42:19 -0500

    second pass at cleaning data
    Signed-off-by: Ayan Sengupta <asengupta.mlg@gmail.com>

--- src/data_clean.py
+++ src/data_clean.py
@@ -8,14 +8,6 @@ from sklearn.preprocessing import StandardScaler
 # Read CSV (listings.csv) into a pandas DataFrame
 listings_df = pd.read_csv('data/raw/listings.csv')
 
-# Display the first few rows of the DataFrame
-# print(listings_df.head())
-# print(listings_df.info())
-# print(listings_df.shape)
-
-# # find unique location in the dataset 
-# print(listings_df['host_location'].unique())
-
 
 # only looking at Asheville, NC, dropping all other locations including empty ones 
 listings_df = listings_df[listings_df['host_location'] == 'Asheville, NC']
@@ -34,6 +26,9 @@ listings_df['has_review_scores'] = listings_df['review_scores_rating'].notna().a
 # replace missing values in review_scores_rating with a 0 
 listings_df['review_scores_rating'] = listings_df['review_scores_rating'].fillna(0)
 
+# convert price column to float and remove the $ sign
+listings_df['price'] = listings_df['price'].replace(r'[\$,]', '', regex=True).astype(float)
+
 # check if accommodates, bathrooms, bedrooms, minumum_nights and review_score_ratings columns have any missing values
 missing_values = [
     listings_df['accommodates'].isnull().sum(),
@@ -47,10 +42,10 @@ missing_values = [
 sum = 0
 for i in missing_values:
     sum += i
-   
-print(sum)
 
-# if no missing value, write the df to a new csv file
+
+
+# if no missing value, look at some descriptive stats for the independent variables selected, write the df to a new csv file
 if price_nullFlag == 0 and sum == 0:
     listings_df.to_csv('data/processed/listings_clean.csv', index=False)
 
